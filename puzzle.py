8b6c099c0a570a6002f656880ddccf8ab40da47b
Alex <kulikov.webdesign@gmail.com>
2024-12-19 23:48:36 -0600

    resolved

--- puzzle.py
+++ puzzle.py
@@ -12,21 +12,47 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    # describe context. Each character is either a knight or a knave
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
+
+    # A says "I am both a knight and a knave."
+    Implication(And(AKnight, AKnave), AKnight),  # if it says true then AKnight
+    Implication(Not(And(AKnight, AKnave)), AKnave),  # if it says lie then AKnave
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    # describe context. Each character is either a knight or a knave
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
+    And(Or(BKnight, BKnave), Not(And(BKnight, BKnave))),
+
+    # A says "We are both knaves."
+    Implication(And(AKnave, BKnave), AKnight),  # if it says true then AKnight
+    Implication(Not(And(AKnave, BKnave)), AKnave),  # if it says lie then AKnave
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    # describe context. Each character is either a knight or a knave
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
+    And(Or(BKnight, BKnave), Not(And(BKnight, BKnave))),
+
+    # A says "We are the same kind."
+    Implication(Or(And(AKnave, BKnave), And(AKnight, BKnight)),
+                AKnight),  # if it says true then AKnight
+    Implication(Not(Or(And(AKnave, BKnave), And(AKnight, BKnight))),
+                AKnave),  # if it says lie then AKnave
+
+    # B says "We are of different kinds."
+    Implication(Or(And(AKnave, BKnight), And(AKnight, BKnave)),
+                BKnight),  # if it says true then BKnight
+    Implication(Not(Or(And(AKnave, BKnight), And(AKnight, BKnave))),
+                BKnave),  # if it says lie then BKnave
+
 )
 
 # Puzzle 3
@@ -35,7 +61,26 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    # describe context. Each character is either a knight or a knave
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
+    And(Or(BKnight, BKnave), Not(And(BKnight, BKnave))),
+    And(Or(CKnight, CKnave), Not(And(CKnight, CKnave))),
+
+    # A says either "I am a knight." or "I am a knave.", but you don't know which.
+    Implication(Or(AKnight, AKnave), AKnight),
+    Implication(Not(Or(AKnight, AKnave)), AKnave),
+
+    # B says "A said 'I am a knave'."
+    Implication(AKnave, BKnight),
+    Implication(Not(AKnave), BKnave),
+
+    # B says "C is a knave."
+    Implication(CKnave, BKnight),
+    Implication(Not(CKnave), BKnave),
+
+    # C says "A is a knight."
+    Implication(AKnight, CKnight),
+    Implication(Not(AKnight), CKnave),
 )
 
 
